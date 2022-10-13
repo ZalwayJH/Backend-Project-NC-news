be6d12f993071b4ae3d6316a252c965e8bbf27fe
ZalwayJH <joelhaughton94@gmail.com>
2022-10-13 09:03:56 +0100

    Completed task 7 which adds the comment_count row to the article object with a value of all the comments with matching article_id numbers

--- Api/controllers/articlesController.js
+++ Api/controllers/articlesController.js
@@ -1,10 +1,13 @@
-const { fetchArticles, updateArticleById } = require("../models/articlesModel");
+const {
+  fetchArticles,
+  updateArticleById,
+} = require("../models/articlesModel.js");
 
 exports.getArticlesId = (req, res, next) => {
   const { article_id } = req.params;
   fetchArticles(article_id)
     .then((articles) => {
-      res.status(200).send({ articles });
+      res.status(200).send(articles);
     })
     .catch((err) => {
       next(err);
